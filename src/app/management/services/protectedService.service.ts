831009aadffdc33b635a1ee01196b1f44e727998
Anthony Benavente <anthony.m.benavente@gmail.com>
2024-07-24 16:42:21 -0400

    added helper parent service class

---
+++ src/app/management/services/protectedService.service.ts
@@ -0,0 +1,28 @@
+import { Observable } from "rxjs";
+import { environment } from "../../../environment";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class ProtectedService {
+    constructor (
+        protected http: HttpClient
+    ) { }
+
+    get<T>(path: string, params?: any): Observable<T> {
+        let httpParams = new HttpParams({ fromObject: params });
+        return this.http.get<T>(`${environment.baseUrl}/${path}`, {
+            params: httpParams,
+        })
+    }
+
+    post<T>(path: string, data: any): Observable<T> {
+        return this.http.post<T>(`${environment.baseUrl}/${path}`, data);
+    }
+
+    delete<T>(path: string): Observable<T> {
+        return this.http.delete<T>(`${environment.baseUrl}/${path}`)
+    }
+
+
+}
