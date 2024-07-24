9fee520be834716aab42f25e1f05b8275e569d5c
Anthony Benavente <anthony.m.benavente@gmail.com>
2024-07-24 16:40:59 -0400

    cleaned up module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SpiceForms } from '../db/spiceForms.enum';
-import { jqxGridComponent, jqxGridModule } from 'jqwidgets-ng/jqxgrid';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { UsersComponent } from './management/users.component';
 import { SpicesComponent } from './management/spices.component';
 import { RouterModule } from '@angular/router';
-import { JwtService } from './auth/services/jwt.service';
-import { IsLoggedInGuard } from './auth/jwtGuard';
 import { HomeComponent } from './management/home.component';
 import { ManagementComponent } from './management/management.component';
 
@@ -16,8 +10,7 @@ import { ManagementComponent } from './management/management.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterModule, HomeComponent, UsersComponent, SpicesComponent, ManagementComponent]
+  imports: [RouterModule, ManagementComponent, HomeComponent, UsersComponent, SpicesComponent]
 })
 export class AppComponent {
-;
 }
