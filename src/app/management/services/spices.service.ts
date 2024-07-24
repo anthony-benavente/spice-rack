22e3937ab537315194f9333dd4d0b2ccd7588034
Anthony Benavente <anthony.m.benavente@gmail.com>
2024-07-24 16:42:09 -0400

    added api calls for spices/users

---
+++ src/app/management/services/spices.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { ProtectedService } from "./protectedService.service";
+import { map, Observable } from "rxjs";
+import { SpiceModel } from "../../../db/spice.model";
+
+interface IGetSpicesResults {
+    limit: number;
+    page: number;
+    results: SpiceModel[];
+    totalPages: number;
+    totalResults: number;
+}
+
+@Injectable()
+export class SpicesService extends ProtectedService {
+
+    getSpices(): Observable<SpiceModel[]> {
+        return this.get<IGetSpicesResults>('spices', { limit: 100, sortBy: 'name' }).pipe(
+            map(data => data.results)
+        );
+    }
+
+    addSpice(spice: SpiceModel): Observable<SpiceModel> {
+        return this.post('spices', spice);
+    }
+}
