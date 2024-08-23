60b0a217ae07da1981066e0b6dbc320b98f43224
Anthony Benavente <anthony@admissionpros.com>
2024-08-23 10:22:15 -0400

    added logout route to handle logging out of site

--- src/app/management/management.routes.ts
+++ src/app/management/management.routes.ts
@@ -2,13 +2,14 @@
 import { Routes } from '@angular/router';
 import { SpicesComponent } from './spices.component';
 import { UsersComponent } from './users.component';
-import { IsLoggedInGuard } from '../auth/jwtGuard';
 import { HomeComponent } from './home.component';
 import { SpiceInventoryComponent } from './spice-inventory.component';
+import { LogoutComponent } from './logout.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'spices', component: SpicesComponent,},
     {path: 'users', component: UsersComponent},
     {path: 'spice-inventory', component: SpiceInventoryComponent},
+    {path: 'logout', component: LogoutComponent},
 ];
